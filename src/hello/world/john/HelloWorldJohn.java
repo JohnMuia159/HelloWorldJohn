d620a6b85992656de0b9b67a50fbc10f54974bb2
jomui8426 <jomui8426@7HKP6Y1.Student.UGDSB.ED>
2017-09-19 15:18:38 -0400

    Changed to hello world kaune

--- src/hello/world/john/HelloWorldJohn.java
+++ src/hello/world/john/HelloWorldJohn.java
@@ -16,7 +16,7 @@ public class HelloWorldJohn {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello World");
+       System.out.println("Hello World kaune");
     }
     
 }
